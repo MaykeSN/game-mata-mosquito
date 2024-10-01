68aa39655a0d68d825fa557ba92a81b067c62b5f
MaykeSN <mayke.souza2002@gmail.com>
2024-09-30 21:38:16 -0300

    feat(render): add mosca random

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 var altura = 0;
 var largura = 0;
 
-function ajustaTamanhoPalcoJogo(){
-    altura = window.innerHeight;
-    largura = window.innerWidth;
+function ajustaTamanhoPalcoJogo() {
+  altura = window.innerHeight;
+  largura = window.innerWidth;
+}
+function addMoscaRandom() {
+    var posicaoX = Math.floor(Math.random() * largura) - 90;
+    var posicaoY = Math.floor(Math.random() * altura) - 90;
+
+    posicaoX = posicaoX < 0 ? 0: posicaoX;
+    posicaoX = posicaoY < 0 ? 0: posicaoY;
+
+    var mosquito = document.createElement("img");
+    mosquito.src = "images/mosca.png";
+    mosquito.classList.add("mosquito_1");
+    mosquito.style.left = posicaoX + 'px';
+    mosquito.style.top = posicaoY + 'px';
+    mosquito.style.position = 'absolute';
+    document.body.appendChild(mosquito);
 }
 
+ajustaTamanhoPalcoJogo();
+addMoscaRandom();
