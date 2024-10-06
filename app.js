997b1b3d86c7c9886bdbd11ed51a1791851d21bf
MaykeSN <mayke.nogueira@grupomulti.com.br>
2024-10-06 20:08:26 -0300

    i need see whats this code change for better

--- app.js
+++ app.js
@@ -1,38 +1,63 @@
+
 var altura = 0;
 var largura = 0;
 
 function ajustaTamanhoPalcoJogo() {
-  altura = window.innerHeight;
-  largura = window.innerWidth;
-}
-function addMoscaRandom() {
-    var posicaoX = Math.floor(Math.random() * largura) - 90;
-    var posicaoY = Math.floor(Math.random() * altura) - 90;
+	altura = window.innerHeight
+	largura = window.innerWidth
 
-    posicaoX = posicaoX < 0 ? 0: posicaoX;
-    posicaoX = posicaoY < 0 ? 0: posicaoY;
-
-    var mosquito = document.createElement("img");
-    mosquito.src = "images/mosca.png";
-    mosquito.classList.add("mosquito_1");
-    mosquito.style.left = posicaoX + 'px';
-    mosquito.style.top = posicaoY + 'px';
-    mosquito.style.position = 'absolute';
-    document.body.appendChild(mosquito);
+	console.log(largura, altura)
 }
 
-function tamanhoAleatorio(){
-  var classe = Math.floor(Math.random() * 3);
-  switch (classe){
-    case 0:
-      return 'mosquito_1'
-    case 1:
-      return 'mosquito_2'
-    case 2: 
-      return 'mosquito_3'
-  }
+ajustaTamanhoPalcoJogo()
+
+
+function posicaoRandomica() {
+
+	var posicaoX = Math.floor(Math.random() * largura) - 90
+	var posicaoY = Math.floor(Math.random() * altura) - 90
+
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+	console.log(posicaoX, posicaoY)
+
+	var mosquito = document.createElement('img')
+	mosquito.src = 'images/mosca.png'
+	mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+	mosquito.style.left = posicaoX + 'px'
+	mosquito.style.top = posicaoY + 'px'
+	mosquito.style.position = 'absolute'
+
+	document.body.appendChild(mosquito)
 
 }
 
-ajustaTamanhoPalcoJogo();
-addMoscaRandom();
+function tamanhoAleatorio() {
+	var classe = Math.floor(Math.random() * 3)
+	
+	switch(classe) {
+		case 0:
+			return 'mosquito_1'
+		
+		case 1:
+			return 'mosquito_2'
+
+		case 2:
+			return 'mosquito_3'
+	}
+}
+
+function ladoAleatorio() {
+	var classe = Math.floor(Math.random() * 2)
+	
+	switch(classe) {
+		case 0:
+			return 'ladoA'
+		
+		case 1:
+			return 'ladoB'
+	}
+}
+
+posicaoRandomica();
