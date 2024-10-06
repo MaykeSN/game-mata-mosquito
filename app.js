492e82ddcfe97eb6753683fb118485e76a6579aa
MaykeSN <mayke.souza2002@gmail.com>
2024-10-06 20:28:50 -0300

    adicionado barras de vida e cronometro

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
 
+  if(document.getElementById('mosquito')     )
+    document.getElementById('mosquito').remove();
+
+
 	var posicaoX = Math.floor(Math.random() * largura) - 90
 	var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -28,7 +32,7 @@ function posicaoRandomica() {
 	mosquito.style.left = posicaoX + 'px'
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
-
+  mosquito.id = "mosquito";
 	document.body.appendChild(mosquito)
 
 }
@@ -59,5 +63,3 @@ function ladoAleatorio() {
 			return 'ladoB'
 	}
 }
-
-posicaoRandomica();
