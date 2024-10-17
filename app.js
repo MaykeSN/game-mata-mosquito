c804dbc6be146f80b156c724da57fc491e92d47b
MaykeSN <mayke.souza2002@gmail.com>
2024-10-16 21:17:38 -0300

    adicionado game over

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function posicaoRandomica() {
 	
 
 	if(vidas > 3){
-		alert('Game over')
+		window.location.href = "fim_de_jogo.html";
 	}
 	else{
 		document.getElementById('v' + vidas).src = "images/coracao_vazio.png";
