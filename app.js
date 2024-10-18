2ebb13061cc27938065036ccf983ef5bf8140147
MaykeSN <mayke.souza2002@gmail.com>
2024-10-17 21:08:40 -0300

    adicionado tela de vitoria

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var cronometro = setInterval(function() {
 	if(tempo < 0){
 		clearInterval(cronometro);
 		clearInterval(criamosca)
-		alert('venceu')
+		window.location.href = "vitoria.html";
 	}
 	else{
 		document.getElementById('cronometro').innerHTML = tempo;
